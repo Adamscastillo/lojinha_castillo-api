5b4cc732988fa2f41ada4befaa00ca85f5a6581f
Adams Castillo <adams.santos.ap@gmail.com>
2022-11-21 10:45:37 -0300

    correcao

--- src/config/auth.config.js
+++ src/config/auth.config.js
@@ -1,9 +1,7 @@
 //*const {env} = require("process")
 
 module.exports = {
-<<<<<<< HEAD
+
   secret:  process.env.APP_SECRET, 
-=======
-  secret:env.APP_SECRET, 
->>>>>>> 4117fd45b525608793e0480e45fa2cf148eb0364
+
 };
