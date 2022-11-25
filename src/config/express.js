d9a3cdf523d3d6511314f9003d1d1b4d4dac410e
Adams Castillo <adams.santos.ap@gmail.com>
2022-11-25 14:45:52 -0300

    remover cors

--- src/config/express.js
+++ src/config/express.js
@@ -2,7 +2,7 @@
 const express = require ('express'); // busca da lib express em node_modules
 const bodyParser = require('body-parser');//vai me dizer com que iremos trabalhar tipo json
 const morgan = require('morgan') //monitora rotas que estao sendo acessadas no app
-const cors = require('cors')//permite a conexao do mesmo local
+
 const app = express();  // executa o express e guardando em uma variavel chamada App
 //import axios from "axios";
 //import qs from "query-string";
@@ -17,7 +17,7 @@ const routerManager = require('../routes') // import da rotas
 
 
 
-app.use(cors()); //chamando o cors
+
 app.use(bodyParser.json()); //executa o bodyPaser para usar o json
 app.use(morgan('dev'))//ambiente de desenvolvimento 
 app.use('/v1', routerManager); // rota geral de versao, vai ser usado depois do Barra
